703d680633102d02d588cdf87caf2677906d461d
Anshika-Shahi <161196082+Anshika-Shahi@users.noreply.github.com>
2024-05-24 16:13:02 +0530

    utilities.py

--- dewwispertranscriber-main/utilities.py
+++ dewwispertranscriber-main/utilities.py
@@ -1,18 +1,15 @@
 import torch
 import ctranslate2
 
-def get_compute_and_platform_info():
-    available_devices = ["cpu"]
-    
+def get_available_devices():
+    devices = ["cpu"]
     if ctranslate2.get_cuda_device_count() > 0:
-        available_devices.append('cuda')
-    
-    return available_devices
+        devices.append("cuda")
+    return devices
 
-def get_supported_quantizations(device_type):
-    types = ctranslate2.get_supported_compute_types(device_type)
-    filtered_types = [q for q in types if q != 'int16']
-    desired_order = ['float32', 'float16', 'bfloat16', 'int8_float32', 'int8_float16', 'int8_bfloat16', 'int8']
+def get_supported_quantization_types(device):
+    supported_types = ctranslate2.get_supported_compute_types(device)
+    filtered_types = [q for q in supported_types if q != "int16"]
+    desired_order = ["float32", "float16", "bfloat16", "int8_float32", "int8_float16", "int8_bfloat16", "int8"]
     sorted_types = [q for q in desired_order if q in filtered_types]
     return sorted_types
-
